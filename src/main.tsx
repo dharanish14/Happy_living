b09d5762c08f003e401a9706e80e5a030f8b1695
dharanish14 <dharaanish@gmail.com>
2026-05-07 21:18:21 +0530

    Fix refresh 404 with SPA fallback

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const redirectParam = '__spa_redirect'
+const url = new URL(window.location.href)
+const redirectTo = url.searchParams.get(redirectParam)
+if (redirectTo) {
+  url.searchParams.delete(redirectParam)
+  window.history.replaceState(null, '', redirectTo)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
